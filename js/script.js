3fef5f2bdec5e171a33587f8c48473f753290ad9
alexboliveira <oalexbruno@gmail.com>
2024-05-12 15:18:35 -0300

    enviar arquivos pc

---
+++ js/script.js
@@ -0,0 +1,49 @@
+//importando card cardQrCode
+import CardQrCode from "../modules/cardQrCode/cardQrCode.js";
+const cardQrCode= new CardQrCode();
+
+
+
+
+
+
+
+//dados da tela do usuario
+const alturaTela = window.innerHeight;
+const larguraTela = window.innerWidth;
+
+
+//const alturaHeader = document.querySelector("body > header").offsetHeight;
+//console.log("altura do header:"+ alturaHeader);
+
+
+//const alturaSectionInicial =  altura - alturaHeader;
+//document.querySelector("body > article > section").style.height = alturaSectionInicial+"px";
+
+
+
+
+
+
+
+
+    
+
+
+
+
+/*
+pessoa.setNome("alex");
+console.log(pessoa.getNome());
+
+pessoa.setNome("aaa");
+
+console.log(pessoa.getNome());
+*/
+
+
+
+
+
+
+
